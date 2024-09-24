fbc2566bf061d7535cc90e5bf43be87b8c150ce3
manixcoder <pathakmanish86@gmail.com>
2024-09-25 03:05:59 +0530

    Adding font awesome icons to iNotebook

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,22 +9,22 @@ const Home = () => {
             <h1>Add a Note </h1>
             <div className="container my-3">
                 <form>
-                    <div class="mb-3">
-                        <label for="title" class="form-label">Title</label>
-                        <input type="test" class="form-control" id="title" />
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    <div className="mb-3">
+                        <label htmlFor="title" className="form-label">Title</label>
+                        <input type="test" className="form-control" id="title" />
+                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                     </div>
-                    <div class="mb-3">
-                        <label for="descripation" class="form-label">Descripation</label>
-                        <input type="text" class="form-control" id="descripation" />
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="tag" class="form-label">Tag</label>
-                        <input type="text" class="form-control" id="tag" />
+                    <div className="mb-3">
+                        <label htmlFor="descripation" className="form-label">Descripation</label>
+                        <input type="text" className="form-control" id="descripation" />
                     </div>
 
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <div className="mb-3">
+                        <label htmlFor="tag" className="form-label">Tag</label>
+                        <input type="text" className="form-control" id="tag" />
+                    </div>
+
+                    <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
             </div>
             <Notes/>
